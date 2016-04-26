1fc5c1f0d6d6ab1079fd8a3b762277fc3a627b10
wuzhi1023 <woozhh@gmail.com>
2016-04-26 08:32:17 -0600

    Function2 done

---
+++ js/function2.js
@@ -0,0 +1,41 @@
+$(function() {
+  $("#radios").radiosToSlider();
+  $("#radios").velocity({scale: [0.7,0.7]});
+  var chart;
+  google.charts.load('current', {'packages':['geochart']});
+  google.charts.setOnLoadCallback(drawRegionsMap);
+
+  $('#option1').click(function() {
+    $.getJSON('averageIncomeMap.json', function (data) {
+      console.log(data);
+      drawJSON(data);
+    });
+  });
+  $('#option2').click(function() {
+    $.getJSON('medianIncomeMap.json', function (data) {
+      drawJSON(data);
+    });
+  });
+  $('#option3').click(function() {
+    $.getJSON('richpeoplePercentageMap.json', function (data) {
+      drawJSON(data);
+    });
+  });
+  $('#option4').click(function() {
+    $.getJSON('UnemploymentMap.json', function (data) {
+      drawJSON(data);
+    });
+  });
+  function drawRegionsMap() {
+    chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+    $.getJSON('averageIncomeMap.json', function (data) {
+      drawJSON(data);
+    });
+  }
+
+  function drawJSON(JSON) {
+    var data = new google.visualization.DataTable(JSON);
+    var options = {'width':800, 'height':600, region: "US", resolution: "provinces"};
+    chart.draw(data, options);
+  }
+});
