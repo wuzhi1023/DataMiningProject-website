7288b1dcc280557446f804c5381710567239208b
wuzhi1023 <woozhh@gmail.com>
2016-04-26 05:40:00 -0600

    2013 income

--- js/function1.js
+++ js/function1.js
@@ -19,18 +19,26 @@ $(function() {
       drawJSON(data);
     });
   });
+  $('#option2').click(function() {
+    console.log('here');
+    $.getJSON('2013Income.json', function (data) {
+      console.log(data);
+      drawJSON(data);
+    });
+  });
   function drawRegionsMap() {
 
     var data = google.visualization.arrayToDataTable([
-      ['Country', 'Popularity'],
-      ['Germany', 0],
-      ['United States', 0],
-      ['Brazil', 0],
-      ['Canada', 0],
-      ['France', 0],
-      ['RU', 0]
+      ['State', 'Average Income']
+      // ['Country', 'Popularity'],
+      // ['Germany', 0],
+      // ['United States', 0],
+      // ['Brazil', 0],
+      // ['Canada', 0],
+      // ['France', 0],
+      // ['RU', 0]
     ]);
-    var options = {'width':800, 'height':600};
+    var options = {'width':800, 'height':600, region: "US", resolution: "provinces"};
     chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
     chart.draw(data, options);
   }
@@ -41,7 +49,7 @@ $(function() {
 
     var toPrint = data.toJSON();
     console.log(toPrint);
-    var options = {'width':800, 'height':600};
+    var options = {'width':800, 'height':600, region: "US", resolution: "provinces"};
     chart.draw(data, options);
   }
 });
