dcb7a0d9253e8564c620241a2ec44dce1f124a30
wuzhi1023 <woozhh@gmail.com>
2016-04-27 05:08:46 -0600

    all three functions done

--- js/function1.js
+++ js/function1.js
@@ -5,50 +5,40 @@ $(function() {
   google.charts.load('current', {'packages':['geochart']});
   google.charts.setOnLoadCallback(drawRegionsMap);
 
-  $("[data-ratio='option1']").click(function() {
-    console.log('here');
-    $.getJSON('example.json', function (data) {
-      console.log(data);
-      drawJSON(data);
-    });
-  });
   $('#option1').click(function() {
-    console.log('here');
-    $.getJSON('example.json', function (data) {
-      console.log(data);
+    $.getJSON('2009Income.json', function (data) {
       drawJSON(data);
     });
   });
   $('#option2').click(function() {
-    console.log('here');
+    $.getJSON('2010Income.json', function (data) {
+      drawJSON(data);
+    });
+  });
+  $('#option3').click(function() {
+    $.getJSON('2011Income.json', function (data) {
+      drawJSON(data);
+    });
+  });
+  $('#option4').click(function() {
+    $.getJSON('2012Income.json', function (data) {
+      drawJSON(data);
+    });
+  });
+  $('#option5').click(function() {
     $.getJSON('2013Income.json', function (data) {
-      console.log(data);
       drawJSON(data);
     });
   });
   function drawRegionsMap() {
-
-    var data = google.visualization.arrayToDataTable([
-      ['State', 'Average Income']
-      // ['Country', 'Popularity'],
-      // ['Germany', 0],
-      // ['United States', 0],
-      // ['Brazil', 0],
-      // ['Canada', 0],
-      // ['France', 0],
-      // ['RU', 0]
-    ]);
-    var options = {'width':800, 'height':600, region: "US", resolution: "provinces"};
     chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-    chart.draw(data, options);
+    $.getJSON('2009Income.json', function (data) {
+      drawJSON(data);
+    });
   }
 
   function drawJSON(JSON) {
-
     var data = new google.visualization.DataTable(JSON);
-
-    var toPrint = data.toJSON();
-    console.log(toPrint);
     var options = {'width':800, 'height':600, region: "US", resolution: "provinces"};
     chart.draw(data, options);
   }
