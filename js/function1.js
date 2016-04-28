b0d94d17079c7ee5b3eb5b4e0e778df43d2937f6
wuzhi1023 <woozhh@gmail.com>
2016-04-28 06:24:54 -0600

    ratio fix for f1

--- js/function1.js
+++ js/function1.js
@@ -10,7 +10,17 @@ $(function() {
       drawJSON(data);
     });
   });
+  $('ins[data-radio="option1"]').click(function() {
+    $.getJSON('2009Income.json', function (data) {
+      drawJSON(data);
+    });
+  });
   $('#option2').click(function() {
+    $.getJSON('2010Income.json', function (data) {
+      drawJSON(data);
+    });
+  });
+  $('ins[data-radio="option2"]').click(function() {
     $.getJSON('2010Income.json', function (data) {
       drawJSON(data);
     });
@@ -20,12 +30,27 @@ $(function() {
       drawJSON(data);
     });
   });
+  $('ins[data-radio="option3"]').click(function() {
+    $.getJSON('2011Income.json', function (data) {
+      drawJSON(data);
+    });
+  });
   $('#option4').click(function() {
     $.getJSON('2012Income.json', function (data) {
       drawJSON(data);
     });
   });
+  $('ins[data-radio="option4"]').click(function() {
+    $.getJSON('2012Income.json', function (data) {
+      drawJSON(data);
+    });
+  });
   $('#option5').click(function() {
+    $.getJSON('2013Income.json', function (data) {
+      drawJSON(data);
+    });
+  });
+  $('ins[data-radio="option5"]').click(function() {
     $.getJSON('2013Income.json', function (data) {
       drawJSON(data);
     });
